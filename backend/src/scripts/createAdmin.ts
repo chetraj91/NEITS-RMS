9c9e04265f22273a74ed789ad0955e8b87cee2af
Nagmastha <nagmastha76@gmail.com>
2026-07-11 15:11:53 +0545

    Fix Prisma client import

--- backend/src/scripts/createAdmin.ts
+++ backend/src/scripts/createAdmin.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "../generated/prisma";
 import { hashPassword } from "../utils/password";
 
 const prisma = new PrismaClient();
